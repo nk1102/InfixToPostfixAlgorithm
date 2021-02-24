472adbd1ceabac341356d1ba2b50a575a3e0decb
nikunj <nikunj.khandelwal_cs19@gla.ac.in>
2021-02-24 20:25:56 +0530

    created a stack which accepts character

--- src/infix/postfix/InfixToPostfix.java
+++ src/infix/postfix/InfixToPostfix.java
@@ -1,9 +1,12 @@
 package infix.postfix;
 
+import java.util.Stack;
+
 public class InfixToPostfix {
     public String infixToPostfix(String infixString) {
     }
     String postfix ="";
+    Stack<Character> stack = new Stack<Character>();
 
 
 }
