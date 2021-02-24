fab6b89c5010438283dd992845ef16f6c56ac01b
nikunj <nikunj.khandelwal_cs19@gla.ac.in>
2021-02-24 21:17:29 +0530

    called main method and executed the algorithm

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,16 @@
 package main;
 
+import infix.postfix.InfixToPostfix;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String infix11 = scanner.nextLine();
+        InfixToPostfix conversion = new InfixToPostfix();
+        String postfix = conversion.infixToPostfix(infix11);
+        System.out.println("postfix  = " + postfix);
 
     }
 }
